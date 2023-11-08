ba456ecc34fcd58a9cc1b5cd2fd6bb4c2908723d
Saidmurod Khamidov <saidmurod217@gmail.com>
2023-11-08 19:53:00 +0500

    fixed code 1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,19 +2,22 @@ void problemSolution1(float consumed_water) {
     float cost = 13;
     // write your code here
     if(consumed_water - 30 >= 0){
-        cost += 30 * 0.4;
+        cost += 30 * (float)0.4;
     } else {
-        cost += consumed_water * 0.4;
+        cost += consumed_water * (float)0.4;
     }
     if(consumed_water - 20 >= 0){
-      cost += 20 * 0.12;
+      cost += 20 * (float)0.12;
     } else{
-      cost += consumed_water * 0.12;
+      cost += consumed_water * (float)0.12;
     }
     if(consumed_water - 10 >= 0){
-        cost += 10 * 1.4;
+        cost += 10 * (float)1.4;
     } else {
-        cost += consumed_water * 1.5;
+        cost += consumed_water * (float)1.4;
+    }
+    if(consumed_water >= 0){
+        cost += consumed_water * (float)1.5;
     }
 
     return cost;

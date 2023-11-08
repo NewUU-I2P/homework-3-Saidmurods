b0433fc7b893f22e8128554d4d321f2af42e97f9
Saidmurod Khamidov <saidmurod217@gmail.com>
2023-11-08 19:45:39 +0500

    solved 1 problem

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,21 @@
 void problemSolution1(float consumed_water) {
-    float cost;
+    float cost = 13;
     // write your code here
+    if(consumed_water - 30 >= 0){
+        cost += 30 * 0.4;
+    } else {
+        cost += consumed_water * 0.4;
+    }
+    if(consumed_water - 20 >= 0){
+      cost += 20 * 0.12;
+    } else{
+      cost += consumed_water * 0.12;
+    }
+    if(consumed_water - 10 >= 0){
+        cost += 10 * 1.4;
+    } else {
+        cost += consumed_water * 1.5;
+    }
 
     return cost;
 }
